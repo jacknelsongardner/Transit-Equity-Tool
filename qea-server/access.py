ae32a93082cc273a6beec61bdea41110cfe78b61
Jack Gardner <shortfilmsbyjack@aol.com>
2024-09-05 10:58:30 -0700

    Added calculate_access function
    Added calculate_access function for calculating access to bus stops

--- qea-server/access.py
+++ qea-server/access.py
@@ -2,38 +2,39 @@ from score import calculate_score
 
 walkable_dist = 10
 
-def calculate_access(stops, routes, dists):
+def calculate_access(stopCount, stops, dists):
     
     stops_weight = 5
-    routes_weight = 4
+    stops_weight = 4
     dist_weight = 2
 
 
-    adjusted_stops = stops
+    adjusted_stops = stopCount
 
-    total_weight = routes_weight + stops_weight + dist_weight
+    total_weight = stops_weight + stops_weight + dist_weight
 
     stops_weight = stops_weight/total_weight
-    routes_weight = routes_weight/total_weight
+    stops_weight = stops_weight/total_weight
     dist_weight = dist_weight/total_weight
 
     
 
 
     # PEOPLES SCORES 
-    routesList = [0, ]
-    stopList = [0, 25, 50, 100]
-    distList = [10, 5, 2.5, 0]
+    stopsList = [0, 3, 6, 10]
+    stopCountList = [0, 25, 50, 100]
+    distList = [0, .1, .25, .5]
 
-    output = calculate_score(vals=[routes, dists, stops], 
-                    weights=[routes_weight, dist_weight, stops_weight], 
-                    trainSets=[routesList, distList, stopList],
-                    names=['routes', 'distance', 'stops'])
+    output = calculate_score(vals=[stops, dists, stopCount], 
+                    weights=[stops_weight, dist_weight, stops_weight], 
+                    trainSets=[stopsList, distList, stopCountList],
+                    names=['stops', 'distance', 'stopCount'])
 
     return output
 
 if __name__ == "__main__":
-    calculate_access(100, 4, )
+    print(calculate_access([50],[4],[.3]))
+
 
 
 
